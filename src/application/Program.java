b4022e1430ba706d6c8935e57a327e13f08f417e
Waldston Santana <waldston.ws@gmail.com>
2023-12-16 11:59:01 -0300

    CheckMate Logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,7 +22,7 @@ public class Program {
 		
 		
 
-		while (true) {
+		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -55,6 +55,9 @@ public class Program {
 			
 			
 		}
+		
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 
 	}
 
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,5 @@
 package chess;
 
-import java.awt.im.InputMethodHighlight;
-import java.text.ParsePosition;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,6 +16,7 @@ public class ChessMatch {
 	private Color currentPlayer;
 	private Board board;
 	private boolean check;
+	private boolean checkMate;
 
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
@@ -40,6 +39,10 @@ public class ChessMatch {
 
 	public boolean getCheck () {
 		return check;
+	}
+	
+	public boolean getCheckMate() {
+		return checkMate;
 	}
 	
 	public ChessPiece[][] getPieces() {
@@ -74,8 +77,12 @@ public class ChessMatch {
 		
 		check = (testCheck(opponent(currentPlayer))) ? true : false;
 		
+		if (testCheckMate(opponent(currentPlayer))) {
+			checkMate = true;
+		}else {
+			nextTurn();
+		}
 		
-		nextTurn();
 
 		return (ChessPiece) capturedPiece;
 	}
@@ -155,6 +162,33 @@ public class ChessMatch {
 
 		}
 		return false;
+	}
+	
+	private boolean testCheckMate(Color color) {
+		if (!testCheck(color)) {
+			return false;
+		}
+		
+		List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece) x).getColor() == color)
+				.collect(Collectors.toList());
+		for(Piece piece : list) {
+			boolean[][] mat = piece.possibleMoves();
+			for (int i = 0; i < board.getRows(); i++) {
+				for (int j = 0; j < board.getColumns(); j++) {
+					if (mat[i][j]) {
+						Position source = ((ChessPiece)piece).getChessPosition().toPosition();
+						Position target = new Position(i, j);
+						Piece capturedPiece = makeMove(source, target);
+						boolean testCheck = testCheck(color);
+						undoMove(source, target, capturedPiece);
+						if (!testCheck) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		return true;
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
